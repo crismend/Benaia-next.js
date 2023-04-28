f5babf1eaace29487ef7289df3c5fa524058afbe
Cristian Mendoza <cfmendoza.audit@gmail.com>
2023-04-28 15:56:34 -0500

    add features

--- src/components/features.js
+++ src/components/features.js
@@ -9,26 +9,56 @@ import {
   Container,
   rem,
 } from '@mantine/core';
-import { IconGauge, IconUser, IconCookie } from '@tabler/icons-react';
+import {
+  Icon3dCubeSphere,
+  IconBuildingCastle,
+  IconHeartHandshake,
+  IconSpeakerphone,
+  IconTractor
+} from '@tabler/icons-react';
 
 const featuresData = [
   {
-    title: 'Extreme performance',
+    title: 'Arquitectura y obra civil',
     description:
       'This dust is actually a powerful poison that will even make a pro wrestler sick, Regice cloaks itself with frigid air of -328 degrees Fahrenheit',
-    icon: IconGauge,
+    icon: IconBuildingCastle,
   },
   {
-    title: 'Privacy focused',
+    title: 'Proyectos de Ingenieria',
     description:
       'People say it can run at the same speed as lightning striking, Its icy body is so cold, it will not melt even if it is immersed in magma',
-    icon: IconUser,
+    icon: Icon3dCubeSphere,
+  },
+  {
+    title: 'Alquiler de maquinaria',
+    description:
+      'They’re popular, but they’re rare. Trainers who show them off recklessly may be targeted by thieves',
+    icon: IconTractor,
+  },
+  {
+    title: 'Consultorias',
+    description:
+      'They’re popular, but they’re rare. Trainers who show them off recklessly may be targeted by thieves',
+    icon: IconSpeakerphone,
+  },
+  {
+    title: 'Apoyo Tecnico',
+    description:
+      'They’re popular, but they’re rare. Trainers who show them off recklessly may be targeted by thieves',
+    icon: IconHeartHandshake,
   },
   {
     title: 'No third parties',
     description:
       'They’re popular, but they’re rare. Trainers who show them off recklessly may be targeted by thieves',
-    icon: IconCookie,
+    icon: Icon3dCubeSphere,
+  },
+  {
+    title: 'No third parties',
+    description:
+      'They’re popular, but they’re rare. Trainers who show them off recklessly may be targeted by thieves',
+    icon: Icon3dCubeSphere,
   },
 ];
 
